287155ec6d3113cd69be04d23219744c381e294d
gustakkk690 <81700712+gustakkk690@users.noreply.github.com>
2021-03-31 12:41:34 -0300

    Add files via upload

---
+++ FADHIL/help.js
@@ -0,0 +1,44 @@
+const help = (prefix, instagram, yt, name, pushname2, user, limitt) => { 
+	return `
+
+══════════════════════﹃
+┃◥⇝ \`\`\`SALVE\`\`\`👋 *${pushname2}*
+┃◥⇝ \`\`\`EU SOU\`\`\` *HINATINHA BOT
+┃◥⇝ \`\`\`Seu limite :\`\`\` *${limitt}*
+┃◥⇝ \`\`\`Verificação.   :\`\`\` *YES*
+┃◥⇝ \`\`\`Premium.   :\`\`\` *NO*
+┃◥⇝ \`\`\`Total de usuários :\`\`\` *${user.length} User*
+┃◥⇝ \`\`\`Total De Doações :\`\`\` *0%* 🙂
+═══════════════════════﹃
+
+┃═══➣MENU√════
+
+┃├❍ *${prefix}allmenu*
+┃├❍ *${prefix}mediamenu*
+┃├❍ *${prefix}creatormenu*
+┃├❍ *${prefix}groupmenu*
+┃├❍ *${prefix}makermenu*
+┃├❍ *${prefix}funmenu*
+┃├❍ *${prefix}nsfwmenu*
+┃├❍ *${prefix}onwermenu*
+┃═══════════════﹃
+
+┃═══➣OUTROS MENUS√════
+
+┃├❍ *${prefix}info*
+┃├❍ *${prefix}donasi*
+┃├❍ *${prefix}owner*
+┃├❍ *${prefix}speed*
+┃├❍ *${prefix}daftar*
+┃├❍ *${prefix}totaluser*
+┃├❍ *${prefix}chatlist*
+┃├❍ *${prefix}grouplist*
+┃├❍ *${prefix}blocklist*
+┃├❍ *${prefix}banlist*
+┃├❍ *${prefix}bahasa*
+══════════════﹃
+
+
+          *HINATINHA*`
+}
+exports.help = help
---
+++ FADHIL/listmenu.js
@@ -0,0 +1,252 @@
+
+const listmenu = (prefix) => { 
+	return ` *LISTA DE COMANDOS HINATINHA√*
+	
+           🔥	H I N A T I N H A🔥
+      	         🔥B O T🔥
+	
+═══➣MENU DE MIDIA√════
+	
+◥⇝ *${prefix}tiktokstalk [usuario] *
+◥⇝ *${prefix}igstalk [ahmd.fdhl_]* 
+◥⇝ *${prefix}insta [Link]*
+◥⇝ *${prefix}instastory [usuario]* 
+◥⇝ *${prefix}ssweb [url]*
+◥⇝ *${prefix}url2img [Url]*
+◥⇝ *${prefix}tiktok*
+◥⇝ *${prefix}fototiktok*
+◥⇝ *${prefix}meme*
+◥⇝ *${prefix}memeindo*
+◥⇝ *${prefix}kbbi*
+◥⇝ *${prefix}wait*
+◥⇝ *${prefix}trendtwit*
+◥⇝ *${prefix}google [ultimas notícias]*
+◥⇝ *${prefix}pinterest*
+◥⇝ *${prefix}play*
+◥⇝ *${prefix}msc1*
+◥⇝ *${prefix}msc2*
+◥⇝ *${prefix}msc3*
+◥⇝ *${prefix}msc4*
+◥⇝ *${prefix}msc5*
+◥⇝ *${prefix}msc6*
+════════════════════
+
+═══➣MENU DE DIVERSÕES√════
+
+◥⇝ *${prefix}anjing* (cachorro)
+◥⇝ *${prefix}kucing* (gato)
+◥⇝ *${prefix}testime*
+◥⇝ *${prefix}hilih* (escolher )
+◥⇝ *${prefix}say*
+◥⇝ *${prefix}apakah* (é)
+◥⇝ *${prefix}kapankah* (quando)
+◥⇝ *${prefix}bisakah* (você pode)
+◥⇝ *${prefix}rate* (avaliar)
+◥⇝ *${prefix}watak* 
+◥⇝ *${prefix}hobby*
+◥⇝ *${prefix}infogempa* (info terremoto)
+◥⇝ *${prefix}infonomor* (informação do número)
+◥⇝ *${prefix}fakta* (facto)
+◥⇝ *${prefix}darkjokes* (piadas negras)
+◥⇝ *${prefix}katacinta* (palavras de amor)
+◥⇝ *${prefix}jadwaltvnow* (agendar tv agora)
+◥⇝ *${prefix}hekerbucin* (hacker bucin)
+◥⇝ *${prefix}katailham* (disse inspiração)
+◥⇝ *${prefix}family100*
+◥⇝ *${prefix}asupan* (ingestão)
+◥⇝ *${prefix}family100*
+◥⇝ *${prefix}kalkulator [13*12]*
+◥⇝ *${prefix}wp [Montanha]*
+══════════════════
+
+═══➣OUTRAS FUNÇÕES√════
+
+◥⇝ *${prefix}translate [en / Como vai você?] *
+◥⇝ *${prefix}pasangan [Fadhil/Salsa]*
+◥⇝ *${prefix}gantengcek [Fadhil]*
+◥⇝ *${prefix}cantikcek [Salsa]*
+◥⇝ *${prefix}artinama [nome]*
+◥⇝ *${prefix}persengay [Bryan]*
+◥⇝ *${prefix}pbucin [nome]*
+◥⇝ *${prefix}bpfont [nome]*
+◥⇝ *${prefix}textstyle [Seu texto]
+◥⇝ *${prefix}jadwaltv [GTV]*
+◥⇝ *${prefix}lirik [I am lady]*
+◥⇝ *${prefix}chord [I am lady]*
+◥⇝ *${prefix}wiki [Adolf Hitler]*
+◥⇝ *${prefix}brainly [pesquisa]*
+◥⇝ *${prefix}resepmasakan [Rawn ]*
+◥⇝ *${prefix}map [Klaten]*
+◥⇝ *${prefix}film [The Walking Death]*
+◥⇝ *${prefix}pinterest [fotos de gatos]*
+◥⇝ *${prefix}infocuaca [Klaten]*
+◥⇝ *${prefix}jamdunia [Indonesia]*
+◥⇝ *${prefix}mimpi [Com ele] *
+◥⇝ *${prefix}infoalamat [informação tarde demais*
+◥⇝ *${prefix}asupan*
+◥⇝ *${prefix}tebakgambar* [adivinhe a imagem]
+◥⇝ *${prefix}caklontong* 
+◥⇝ *${prefix}family100*
+◥⇝ *${prefix}kalkulator [13*12]*
+◥⇝ *${prefix}wp [Montanha]*
+◥⇝ *${prefix}jadwalsholat [cronograma]*
+◥⇝ *${prefix}quran* [Alcorão]
+◥⇝ *${prefix}quransurah [1 ]*
+◥⇝ *${prefix}tafsir [1/5]*
+◥⇝ *${prefix}becrypt [string]*
+◥⇝ *${prefix}encode64 [string]*
+◥⇝ *${prefix}decode64 [encrypt]*
+◥⇝ *${prefix}encode32 [string]*
+◥⇝ *${prefix}decode32 [encrypt]*
+◥⇝ *${prefix}encbinary [string]*
+◥⇝ *${prefix}decbinary [encrypt]*
+◥⇝ *${prefix}encoctal [string]*
+◥⇝ *${prefix}decoctal [encrypt]*
+◥⇝ *${prefix}hashidentifier [Encrypt Hash]*
+◥⇝ *${prefix}dorking [dork]*
+◥⇝ *${prefix}pastebin [teks]*
+◥⇝ *${prefix}tinyurl [link]*
+◥⇝ *${prefix}bitly [link]*
+═══════════════════
+
+═══➣MENU DE PRODUÇÃO√════
+
+◥⇝ *${prefix}sticker*
+◥⇝ *${prefix}quotemaker [tx/wtrmk/tema]*
+◥⇝ *${prefix}nulis [nama/kelas/text]*
+◥⇝ *${prefix}trigger [reply image]*
+◥⇝ *${prefix}rip [reply image]*
+◥⇝ *${prefix}wasted [reply image]*
+◥⇝ *${prefix}cphlogo [nome]*
+◥⇝ *${prefix}cglitch [nome]*
+◥⇝ *${prefix}cpubg [nome]*
+◥⇝ *${prefix}cml [nome]*
+◥⇝ *${prefix}tahta [Salsa]*
+◥⇝ *${prefix}croman [nome dan Salsa]*
+◥⇝ *${prefix}cthunder [nome]*
+◥⇝ *${prefix}cbpink [nome]*
+◥⇝ *${prefix}cmwolf [nome]*
+◥⇝ *${prefix}csky [nome]*
+◥⇝ *${prefix}cwooden [nome]*
+◥⇝ *${prefix}cflower [nome]*
+◥⇝ *${prefix}clove [nome]*
+◥⇝ *${prefix}ccrossfire [nome]*
+◥⇝ *${prefix}cnaruto [nome]*
+◥⇝ *${prefix}cparty [nome]*
+◥⇝ *${prefix}cshadow [nome]*
+◥⇝ *${prefix}cminion [nome]*
+◥⇝ *${prefix}cneon [nome]*
+◥⇝ *${prefix}cneon2 [nome]*
+◥⇝ *${prefix}cneongreen [nome]*
+◥⇝ *${prefix}c3d [nome]*
+◥⇝ *${prefix}csky [nome]*
+◥⇝ *${prefix}tts [id Haii]*
+◥⇝ *${prefix}ttp [nome]*
+◥⇝ *${prefix}slide [nome]*
+◥⇝ *${prefix}stiker*
+◥⇝ *${prefix}gifstiker*
+◥⇝ *${prefix}toimg*
+◥⇝ *${prefix}img2url*
+◥⇝ *${prefix}nobg*
+◥⇝ *${prefix}tomp3*
+◥⇝ *${prefix}ocr*
+═══════════════════
+
+═══➣MENU PARA GRUPO√════
+
+◥⇝ **${prefix}modeanime [On/Off]*
+◥⇝ *${prefix}naruto*
+◥⇝ *${prefix}minato*
+◥⇝ *${prefix}boruto*
+◥⇝ *${prefix}hinata*
+◥⇝ *${prefix}sakura*
+◥⇝ *${prefix}sasuke*
+◥⇝ *${prefix}kaneki*
+◥⇝ *${prefix}toukachan*
+◥⇝ *${prefix}rize*
+◥⇝ *${prefix}akira*
+◥⇝ *${prefix}itori*
+◥⇝ *${prefix}kurumi*
+◥⇝ *${prefix}miku*
+◥⇝ *${prefix}anime*
+◥⇝ *${prefix}animecry*
+◥⇝ *${prefix}neonime*
+◥⇝ *${prefix}animekiss*
+◥⇝ *${prefix}wink*
+◥⇝ *${prefix}welcome [On/Off]*
+◥⇝ *${prefix}grup [buka/tutup]*
+◥⇝ *${prefix}antilink [on/off*
+◥⇝ *${prefix}ownergrup*
+◥⇝ *${prefix}setpp*
+◥⇝ *${prefix}infogc*
+◥⇝ *${prefix}add*
+◥⇝ *${prefix}kick*
+◥⇝ *${prefix}promote*
+◥⇝ *${prefix}demote*
+◥⇝ *${prefix}setname*
+◥⇝ *${prefix}setdesc*
+◥⇝ *${prefix}linkgrup*
+◥⇝ *${prefix}tagme*
+◥⇝ *${prefix}hidetag*
+◥⇝ *${prefix}tagall*
+◥⇝ *${prefix}mentionall*
+◥⇝ *${prefix}fitnah*
+◥⇝ *${prefix}listadmin*
+◥⇝ *${prefix}openanime*
+◥⇝ *${prefix}edotense*
+◥⇝ *${prefix}nsfw [On/Off]*
+◥⇝ *${prefix}nsfwloli*
+◥⇝ *${prefix}nsfwblowjob*
+◥⇝ *${prefix}nsfwneko*
+◥⇝ *${prefix}nsfwtrap*
+◥⇝ *${prefix}hentai*
+◥⇝ *${prefix}simih [On/Off]*
+══════════════════════
+
+═══➣MENU DE PROPRIETÁRIO√════
+
+◥⇝ *${prefix}addprem [mencione]*
+◥⇝ *${prefix}removeprem [mencions]*
+◥⇝ *${prefix}setppbot*
+◥⇝ *${prefix}setreply*
+◥⇝ *${prefix}bc*
+◥⇝ *${prefix}bcgc*
+◥⇝ *${prefix}ban*
+◥⇝ *${prefix}unban*
+◥⇝ *${prefix}block*
+◥⇝ *${prefix}unblock*
+◥⇝ *${prefix}clearall*
+◥⇝ *${prefix}delete*
+◥⇝ *${prefix}clone*
+◥⇝ *${prefix}getses*
+◥⇝ *${prefix}leave*
+◥⇝ *${prefix}setprefix*
+═════════════════
+
+═══➣MENU PREMIUM√════
+
+◥⇝ *${prefix}playmp3 [I am Lady]*
+◥⇝ *${prefix}fb [link video]*
+◥⇝ *${prefix}snack [link snack video]*
+◥⇝ *${prefix}ytmp3 [link yt]*
+◥⇝ *${prefix}ytmp4 [link yt]*
+◥⇝ *${prefix}joox [Ultimate Monologue]*
+◥⇝ *${prefix}smule [Link Video Smule]*
+◥⇝ *${prefix}cersex*
+◥⇝ *${prefix}asupan*
+◥⇝ *${prefix}xxx [nome]*
+◥⇝ *${prefix}pornhub* 
+◥⇝ *${prefix}hentai [Aleatório]*
+◥⇝ *${prefix}ban [Tag do usuario]*
+◥⇝ *${prefix}unban [Tag do usuario]*
+◥⇝ *${prefix}bc [texto]*
+◥⇝ *${prefix}asupan*
+*_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-*
+            
+
+           🇧🇷HINATINHA🇧🇷
+
+	🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+         ` 
+	}
+exports.listmenu = listmenu
